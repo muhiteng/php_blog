227292346fa9a9507231d6d6213d7f6368803f8d
muhiteng <muh.it.eng.19841984@gmail.com>
2021-12-26 11:55:22 -0800

    03 commit init show list posts

--- view/posts/index.php
+++ view/posts/index.php
@@ -1,1 +1,9 @@
-List of posts here
+List of posts here: <br>
+<?php foreach($data["posts"] as $post) {?>
+    <?php echo $post["id"]; ?> -
+    <?php echo $post["title"]; ?> -
+    <?php echo $post["text"]; ?> -
+
+
+    <hr/>
+<?php } ?>
